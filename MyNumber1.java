1ceb009d4c0070efbbdb37addbe5970ed889330e
tranlinh1709 <46080352+tranlinh1709@users.noreply.github.com>
2019-01-08 20:50:52 +0700

    Add files via upload
    * Viết class tên là MyNumber với các đạc điểm sau
     * - Thuộc tính
     * + int number - chứa số nhập vào từ constructor
     * - Phương thức
     * + MyNumber(int n) - Khởi tạo cho thuộc tính number;
     * + ArrayList Get_Sochan() - Trả ra danh sách tất cả các số chẵn nhỏ hơn number
       + ArrayList get_SoLe() - Trả ra danh sách tất cả các số lẻ nhỏ hơn nyumber
       + ArrayList get_SoTongHop() - Trả ra một danh sách gồm tổng các số chẵn lẻ ở 
       vị trí tương ứng, trong trường hợp khuyết 1 trong 2 số thì giữ nguyên số còn lại
     - Viết Test cho 03 methods này

---
+++ MyNumber1.java
@@ -0,0 +1,52 @@
+package pktest;
+
+import java.util.ArrayList;
+
+import javax.print.attribute.IntegerSyntax;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class MyNumber1 {
+public int number;
+public int MyNumber (int n){
+	this.number = n;
+	return n;
+}
+	//Trả ra danh sách tất cả các số chẵn nhỏ hơn number
+public ArrayList Get_Sochan(){
+ArrayList Get_Sochan = new ArrayList();
+for ( int i = 0; i < number; i++) {
+	if (number%2==0) {
+		Get_Sochan.add(i);		
+	}
+}
+	return Get_Sochan;
+}
+	//lay ra so le
+public ArrayList Get_Sole(){
+ArrayList Get_Sole = new ArrayList();
+	for (int j = 0; j < number; j++) {
+	if (number%2==1) {
+		Get_Sole.add(j);
+	}
+	}
+	return Get_Sole;
+}
+	//tinh tong
+public ArrayList Get_Sotonghop(){
+	ArrayList Sotonghop = new ArrayList();
+	ArrayList sochan = Get_Sochan();
+	ArrayList sole = Get_Sole();
+	for (int i = 0; i < number ; i++){
+//	int chan = Integer.parseInt(sochan.get(k).toString());
+//  int le = Integer.parseInt(sole.get(k).toString());
+//	int tong = chan + le;
+//	int i = k;
+//	for (i = 0; i < number; i++) {
+		Sotonghop.add(sochan[i]+sole[i]);
+	}
+		
+	}
+	    
+}
+
